dbd699dc06bdaca8b36f04f7c4c91506d7c92bef
ConorPai <paiconor@PaiConordeMacBook-Pro.local>
2017-05-08 16:25:39 +0800

    优化代码

--- PlantRecognitionWebAPI.py
+++ PlantRecognitionWebAPI.py
@@ -1,12 +1,16 @@
 #coding=utf-8
 
 import sys
+default_encoding = 'utf-8'
+if sys.getdefaultencoding() != default_encoding:
+    reload(sys)
+    sys.setdefaultencoding(default_encoding)
 
 import tensorflow as tf
-import json
-from flask import Flask, request, jsonify
+from flask import Flask, request
 app = Flask(__name__)
 
+#加载识别标签翻译字典
 dictionary = {}
 ftotal = open('./dictionarys.txt', 'r')
 line = ftotal.readline()
@@ -16,6 +20,7 @@ while line:
     line = ftotal.readline()
 ftotal.close()
 
+#加载识别标签
 labels = []
 for label in tf.gfile.GFile("output_labels.txt"):
     labels.append(label.rstrip())
@@ -33,18 +38,23 @@ softmax_tensor = sess.graph.get_tensor_by_name('final_result:0')
 def change_avatar():
     if request.method == 'POST':
         file = request.files['file']
+
+        #构建图片文件存储路径
         photofile = '/Users/paiconor/Downloads/uploadPhoto/' + file.filename
+
+        #保存图片文件
         file.save(photofile)
 
+        #返回植物图片识别结果
         return plantRecognition(photofile), 200
 
 def plantRecognition(plantFile):
 
+    #打开植物图片进行识别
     image = tf.gfile.FastGFile(plantFile, 'rb').read()
-
     predict = sess.run(softmax_tensor, {'DecodeJpeg/contents:0': image})
 
-    # 根据分类概率进行排序
+    #根据分类概率进行排序
     top = predict[0].argsort()[-len(predict[0]):][::-1]
 
     result = ''
@@ -53,9 +63,15 @@ def plantRecognition(plantFile):
         human_string = dictionary[labels[index]].decode("utf-8")
         score = predict[0][index]
 
+        #识别率低于0.01即跳出
+        if score < 0.0099:
+            break
+
+        #拼写识别结果
         temp = human_string + ':' + '%(p).2f'%{'p':score * 100} + '%'
         result += temp.decode("utf-8") + '\n'
 
+        #只返回识别前8个结果
         count -= 1
         if count == 0:
             break
@@ -65,10 +81,4 @@ def plantRecognition(plantFile):
 
 
 if __name__ == '__main__':
-
-    default_encoding = 'utf-8'
-    if sys.getdefaultencoding() != default_encoding:
-        reload(sys)
-        sys.setdefaultencoding(default_encoding)
-
     app.run(host='192.168.1.101', port=5000)
